47b7c599e5e6fed9ee35046f072f6a44c9842c46
Scott <ssanford@mediafly.com>
2018-12-02 21:39:38 -0600

    update tab names

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+import { SummaryPage } from '../pages/summary/summary';
+import { TransactionsPage } from '../pages/transactions/transactions';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -14,9 +14,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    SummaryPage,
+    TransactionsPage,
     ContactPage,
-    HomePage,
     TabsPage
   ],
   imports: [
@@ -26,9 +26,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    SummaryPage,
+    TransactionsPage,
     ContactPage,
-    HomePage,
     TabsPage
   ],
   providers: [
--- src/pages/summary/summary.ts
+++ src/pages/summary/summary.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-summary',
+  templateUrl: 'summary.html'
 })
-export class AboutPage {
+export class SummaryPage {
 
   constructor(public navCtrl: NavController) {
 
