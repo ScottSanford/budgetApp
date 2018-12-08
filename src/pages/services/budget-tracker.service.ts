a7f39ae34e1edb30e171de278d5651543e2f5564
Scott <ssanford@mediafly.com>
2018-12-08 16:50:24 -0500

    add logic for returning daily remaining budget

--- src/pages/services/budget-tracker.service.ts
+++ src/pages/services/budget-tracker.service.ts
@@ -1,12 +1,48 @@
-import { Injectable } from "@angular/core"
+import { Injectable } from '@angular/core'
+import _ from 'lodash'
+import moment from 'moment'
 
 @Injectable()
 export class BudgetTrackerService {
-  // Save Budget Income
-  public budgetIncome: number = 0
-  public budgetExpenses: Budget.Expense[] = []
+	// Save Budget Income
+	public budgetIncome: number = 0
+	// Store budget expenses in an array
+  	public budgetExpenses: Budget.Expense[] = []
 
-  addIncome() {
+	getIncome(amount) {
+		this.budgetIncome = amount
+	}
 
+	addExpenseToBudget(expense: Budget.Expense) {
+		this.budgetExpenses.push(expense)
+	}
+
+	getRemainingBudgetBalance(): number {
+		if (this.budgetExpenses.length >= 1) {
+			const expenseAmounts = _.map(this.budgetExpenses, expense => expense.amount)
+			const expenseTotal = _.sum(expenseAmounts)
+      const remainingBalance = this.budgetIncome - expenseTotal
+			return remainingBalance
+		}
+		// If just the income is entered, show this in the total
+		else {
+			return this.budgetIncome
+		}
+	}
+
+  getRemainingTimeBalance() {
+    const todayDate: any = moment().date()
+    const numDaysInMonth: any = moment().daysInMonth()
+    const remainingDaysInMonth: number = numDaysInMonth - todayDate
+
+    return {
+      daily: this.getRemainingDailyBalance(remainingDaysInMonth),
+      weekly: 'weekly'
+    }
+  }
+
+  getRemainingDailyBalance(remainingDays) {
+    const remainingBalance = this.getRemainingBudgetBalance() / remainingDays
+    return parseFloat(remainingBalance.toFixed(2))
   }
 }
--- src/pages/tracker/tracker.ts
+++ src/pages/tracker/tracker.ts
@@ -3,40 +3,47 @@ import { NavController, NavParams } from 'ionic-angular';
 import { BudgetTrackerService } from '../services/budget-tracker.service';
 
 @Component({
-  selector: 'page-tracker',
-  templateUrl: 'tracker.html'
+	selector: 'page-tracker',
+	templateUrl: 'tracker.html'
 })
 export class TrackerPage implements OnInit {
-  protected budgetName: string
-  protected expenseName: string
-  protected expenseValue: number
-  protected incomeValue: number
-  protected remainingBudgetTotal: number = 0
-  protected showIncomeNumber: boolean
+	protected budgetName: string
+	protected expenseName: string
+	protected expenseValue: number
+	protected incomeValue: number
+	protected remainingBudgetTotal: number
+	protected remainingDailyBalance: number
+	protected showIncomeNumber: boolean
 
-  constructor(
-    private navCtrl: NavController,
-    private navParams: NavParams,
-    private budgetTrackerService: BudgetTrackerService
-  ) {}
+	constructor(
+		private navCtrl: NavController,
+		private navParams: NavParams,
+		private budgetTrackerService: BudgetTrackerService
+	) {}
 
-  ngOnInit() {
-    this.showIncomeNumber = false
-    this.budgetName = this.navParams.data.budget.name
-  }
+	ngOnInit() {
+		this.showIncomeNumber = false
+		this.budgetName = this.navParams.data.budget.name
+	}
 
-  protected convertToNumber(event):number {
-    return +event
-  }
+	protected convertToNumber(event):number {
+		return +event
+	}
 
-  onEnterIncome() {
-    // Save income value to reusable service
-    this.budgetTrackerService.budgetIncome = this.incomeValue
-    this.showIncomeNumber = true
-  }
+	onEnterIncome() {
+		// Save income value to reusable service
+		this.budgetTrackerService.getIncome(this.incomeValue)
+		this.remainingBudgetTotal = this.budgetTrackerService.getRemainingBudgetBalance()
+		this.showIncomeNumber = true
+	}
 
-  onAddExpense(expenses: Budget.Expense) {
-    console.log('expense', expenses)
-  }
+	onAddExpense(expense: Budget.Expense) {
+		this.budgetTrackerService.addExpenseToBudget(expense)
+		this.budgetTrackerService.getRemainingBudgetBalance()
+    this.remainingBudgetTotal = this.budgetTrackerService.getRemainingBudgetBalance()
+
+    // Daily Time Balances
+    this.remainingDailyBalance = this.budgetTrackerService.getRemainingTimeBalance().daily
+	}
 
 }
